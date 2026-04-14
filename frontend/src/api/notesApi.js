b451ee8762b19a34067f92dd629aa82807b51ec9
Phaneendra Kanduri <kphaneendra2005@gmail.com>
2026-04-14 13:09:18 +0530

    fix production API URL

--- frontend/src/api/notesApi.js
+++ frontend/src/api/notesApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: 'https://markdown-notes-app-umyr.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 10000,
 });
@@ -10,15 +10,10 @@ const unwrap = (response) => response.data.data;
 
 export const notesApi = {
   getAll: () => api.get('/notes').then(unwrap),
-
   getById: (id) => api.get(`/notes/${id}`).then(unwrap),
-
   search: (q) => api.get('/notes/search', { params: { q } }).then(unwrap),
-
   create: (note = {}) =>
     api.post('/notes', { title: 'Untitled Note', content: '', ...note }).then(unwrap),
-
   update: (id, patch) => api.put(`/notes/${id}`, patch).then(unwrap),
-
   remove: (id) => api.delete(`/notes/${id}`).then(unwrap),
 };
